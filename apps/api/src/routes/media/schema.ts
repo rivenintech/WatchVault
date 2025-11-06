57a3c40faf37a98b5af2e234a69feb305387d782
rivenintech <contact@rivenintech.com>
2025-11-06 18:33:30 +0100

    fix(api): tmdb data schema adjustments
    - Allow nullable poster_path in Recommendations schema.
    - Adjust Credits schema to handle different character role structures.

--- apps/api/src/routes/media/schema.ts
+++ apps/api/src/routes/media/schema.ts
@@ -1,22 +1,31 @@
 import * as v from "valibot";
 
 export const Recommendations = v.object({
-    results: v.array(v.object({ media_type: v.union([v.literal("movie"), v.literal("tv")]), id: v.number(), poster_path: v.string() })),
+    results: v.array(v.object({ media_type: v.union([v.literal("movie"), v.literal("tv")]), id: v.number(), poster_path: v.nullable(v.string()) })),
 });
 
 export const Credits = v.object({
     cast: v.array(
-        v.object({
-            id: v.number(),
-            profile_path: v.nullable(v.string()),
-            name: v.string(),
-            roles: v.array(
+        v.intersect([
+            v.object({
+                id: v.number(),
+                profile_path: v.nullable(v.string()),
+                name: v.string(),
+                order: v.number(),
+            }),
+            v.union([
+                v.object({
+                    roles: v.array(
+                        v.object({
+                            character: v.string(),
+                        })
+                    ),
+                }),
                 v.object({
                     character: v.string(),
-                })
-            ),
-            order: v.number(),
-        })
+                }),
+            ]),
+        ])
     ),
 });
 
